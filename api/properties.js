f6c9ed280fb976e6d93028fd8b585536bd3303b1
Sns Charmie <charlsoluwapelumi@gmail.com>
2022-05-16 08:08:49 +0100

    View Specific

--- api/properties.js
+++ api/properties.js
@@ -56,4 +56,19 @@ router.patch('/property/:id', authenticateToken, async (req, res) => {
 })
 
 
+//GET /property/<:property-id>: Get a specific property by ID
+router.get('/property/:id', authenticateToken, async (req, res) => {
+    const user = req.user
+    
+    const propertyId = req.params.id
+
+    const property =  await Property.query().findById(propertyId)
+
+
+    return res.json({
+        status: 'success',
+        data:  property
+    })
+})
+
 module.exports = router
